18ecc71ecde8c0b1c2f6144fcb06b9e1bb19f3cd
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2020-03-11 21:24:53 -0400

    Added to ListingController; Added ListingService

--- SpringData/Project2/src/main/java/com/revature/controller/ListingController.java
+++ SpringData/Project2/src/main/java/com/revature/controller/ListingController.java
@@ -3,32 +3,57 @@ package com.revature.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 //TODO Modify localhost
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
-import com.revature.dao.ListingDao;
 import com.revature.model.Listing;
+import com.revature.service.ListingService;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class ListingController {
 	@Autowired
-	ListingDao listingDao;
+	ListingService listingService;
 	
-	@GetMapping("/listing.app")
-	public @ResponseBody Optional<Listing>  findListingById(Integer id) {
-		return listingDao.findById(id);		
+	@GetMapping("/listing")
+	public @ResponseBody Listing  findListingById(Integer id) {
+		return listingService.findById(id);		
+	}
+	/*
+	@GetMapping("/listingAll")
+	public @ResponseBody Listing  findListingById(Integer id) {
+		return listingService.find;		
+	}
+	*/
+	
+	@GetMapping("/test/**")
+	public String handleGet() {
+		return "Success!";
 	}
 	
-	@PostMapping("/application")
-	public Listing createApplication(@RequestBody Listing listing) {
-		return listingDao.save(listing);
+		
+	@ExceptionHandler(HttpClientErrorException.class)
+	public ResponseEntity<String> handleClientError(HttpClientErrorException e) {
+		return ResponseEntity
+				.status(e.getStatusCode())
+				.body(String.format("{\"message\": \"%s\"}", e.getMessage()));
 	}
 
+	@PostMapping("/newlisting")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Listing create(@RequestBody Listing listing) {
+		return listingService.create(listing);
+	}
+	
 }
