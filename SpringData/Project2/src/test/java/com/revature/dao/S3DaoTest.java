568ecadeadc84abfdf0e595aee5f3e550f8b18f9
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2020-03-12 21:56:19 -0400

    S3

--- SpringData/Project2/src/test/java/com/revature/dao/S3DaoTest.java
+++ SpringData/Project2/src/test/java/com/revature/dao/S3DaoTest.java
@@ -3,22 +3,21 @@ package com.revature.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class S3DaoTest {
-	S3Dao dao;
+	S3Dao dao = new S3Dao();
 	
 	@Before
 	public void setUp() throws Exception {
-		 dao = new S3Dao();
+		  
 	}
 
 	 @Test 
-	 public void setReceiptTest () { assertTrue(dao.setFileInS3("reimbId_UserId.jpg")==1); }
+	 public void setReceiptTest () { assertTrue(dao.setFileInS3("AustralianCattleDog_JimHutchins.jpg")==1); }
 	 
 	@Test
-	public void getFileFromS3Test() {assertTrue(dao.getFileFromS3("reimbId_UserId.jpg")==1);}	
+	public void getFileFromS3Test() {assertTrue(dao.getFileFromS3("AustralianCattleDog_JimHutchins.jpg")==1);}	
 	  
 
 }
